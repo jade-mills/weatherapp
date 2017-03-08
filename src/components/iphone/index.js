d0df63fb6728d7ac4102082de0eb1af3cfdaae80
Jade Mills <jade.ellen.mills@gmail.com>
2017-03-08 21:15:57 +0000

    Fix Icon

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -9,8 +9,6 @@ import $ from 'jquery';
 import Button from '../button';
 
 export default class Iphone extends Component {
-//var Iphone = React.createClass({
-
 	// a constructor with initial set states
 	constructor(props){
 		super(props);
@@ -24,12 +22,14 @@ export default class Iphone extends Component {
 	fetchWeatherData = () => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
 		var url = "http://api.wunderground.com/api/6521e73cdd976f11/conditions/q/UK/London.json";
+
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
 			success : this.parseResponse,
 			error : function(req, err){ console.log('API call failed ' + err); }
-		})
+		});
+
 		// once the data grabbed, hide the button
 		this.setState({ display: false });
 	}
@@ -38,25 +38,26 @@ export default class Iphone extends Component {
 	render() {
 		// check if temperature data is fetched, if so add the sign styling to the page
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
-		const windStyles = this.state.wind ? '${style.windspeed} ${style.filled}' : style.windspeed;
-		const rainStyles = this.state.rain ? '${style.rainfall} ${style.filled}' : style.rainfall;
-		const humidityStyles = this.state.humidity ? '${style.humidity} ${style.filled}' : style.humidity;
-		
-		
+
 		// display all weather data
 		return (
 			<div class={ style.container }>
-				<div class={ style.header }>
-					<div class={ style.city }>{ this.state.locate }</div>
-					<div class={ style.conditions }>{ this.state.cond }</div>
-					<span class={ tempStyles }>{ this.state.temp }</span>
-					<div class={ windStyles}>{this.state.wind}</div>
-					<div class={ rainStyles}>{this.state.rain}</div>
-					<div class={ humidityStyles}>{this.state.humidity}</div>
-					<img class={style.icon} src= { this.state.icon}></img>
-				</div>
+				{ !this.state.display &&
+					<div class={ style.header }>
+
+						<div class={ style.city }>{ this.state.locate }</div>
+						<div class={ style.conditions }>{ this.state.cond }</div>
+						<span class={ tempStyles }>{ this.state.temp }</span>
+						<div>{this.state.wind}</div>
+						<div>{this.state.rain}</div>
+						<div>{this.state.humidity}</div>
+						<img class={style.icon} src= { this.state.icon}></img>
+					</div>
+				}
+
 				<div class={ style.details }></div>
-				<div class= { style_iphone.container }> 
+
+				<div class= { style_iphone.container }>
 					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData }/ > : null }
 				</div>
 			</div>
@@ -70,8 +71,8 @@ export default class Iphone extends Component {
 		var wind_string = parsed_json['current_observation']['wind_string'];
 		var precip_today_metric = parsed_json['current_observation']['precip_today_metric'];
 		var relative_humidity = parsed_json['current_observation']['relative_humidity'];
-		var icon_url = "http://icons.wxug.com/i/c/h/" + parsed_json['current_observation']['icon'] + ".gif";
-		
+		var icon_url = parsed_json['current_observation']['icon_url'];
+
 
 		// set states for fields so they could be rendered later on
 		this.setState({
@@ -82,6 +83,6 @@ export default class Iphone extends Component {
 			rain: precip_today_metric,
 			humidity: relative_humidity,
 			icon: icon_url
-		});      
+		});
 	}
 }
